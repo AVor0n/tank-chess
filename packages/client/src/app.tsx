9a9af1110cc9feb370cfc71f04002c17412d15cf
vakulovv <77400624+vakulovv@users.noreply.github.com>
2024-01-09 23:58:49 +0300

    feat(Fullscreen API): #Y32 Добавить минимум одно Web API (#24)
    Co-authored-by: vetalvakulov <v.vakulov@bftcom.com>

--- packages/client/src/app.tsx
+++ packages/client/src/app.tsx
@@ -1,7 +1,9 @@
 'use client'
 import { ThemeProvider } from '@gravity-ui/uikit'
+import { useEffect } from 'react'
 import { RouterProvider } from 'react-router-dom'
 import Error from '@components/error'
+import { toggleFullScreen } from '@utils/webApi'
 import withAuthInfo from 'hoc/withAuthInfo'
 import router from './router'
 import registerServiceWorker from './utils/serviceWorker'
@@ -10,11 +12,27 @@ if (process.env.NODE_ENV === 'production') {
   registerServiceWorker()
 }
 
-const App = () => (
-  <ThemeProvider theme="light">
-    <RouterProvider router={router} />
-    <Error />
-  </ThemeProvider>
-)
+const App = () => {
+  useEffect(() => {
+    const onPressKey = (event: KeyboardEvent) => {
+      if (event.ctrlKey && event.key === 'Enter') {
+        toggleFullScreen(null)
+      }
+    }
+
+    document.addEventListener('keyup', onPressKey)
+
+    return () => {
+      document.removeEventListener('keyup', onPressKey)
+    }
+  }, [])
+  return (
+    <ThemeProvider theme="light">
+      <RouterProvider router={router} />
+      <Error />
+    </ThemeProvider>
+  )
+}
+
 
 export default withAuthInfo(App)
