5a880d0f28429823fce69b9466db13cb72dd24a0
vakulovv <77400624+vakulovv@users.noreply.github.com>
2024-01-09 23:23:27 +0300

    feat(serviceWorker): #Y30 Добавить Service Workers (#22)
    Co-authored-by: vetalvakulov <v.vakulov@bftcom.com>

--- packages/client/src/app.tsx
+++ packages/client/src/app.tsx
@@ -4,6 +4,11 @@ import { RouterProvider } from 'react-router-dom'
 import Error from '@components/error'
 import withAuthInfo from 'hoc/withAuthInfo'
 import router from './router'
+import registerServiceWorker from './utils/serviceWorker'
+
+if (process.env.NODE_ENV === 'production') {
+  registerServiceWorker()
+}
 
 const App = () => (
   <ThemeProvider theme="light">
