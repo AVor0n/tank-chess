34b97b818d54cf1561c4635bf7d3a749adb5966e
vetalvakulov <v.vakulov@bftcom.com>
2024-01-28 16:55:45 +0300

    feat(SSR): #Y36 Настроить Redux и Router в SSR. Добавил Fullscreen API

--- packages/client/src/app.tsx
+++ packages/client/src/app.tsx
@@ -1,7 +1,8 @@
 'use client'
 import { ThemeProvider } from '@gravity-ui/uikit'
-import { type FC } from 'react'
+import { type FC, useEffect } from 'react'
 import Error from '@components/error'
+import { toggleFullScreen } from '@utils/webApi'
 import withAuthInfo from './hoc/withAuthInfo'
 import registerServiceWorker from './utils/serviceWorker'
 
@@ -13,11 +14,27 @@ interface Props {
   RouterProvider?: React.ComponentType
 }
 
-const App: FC<Props> = ({ RouterProvider }) => (
-  <ThemeProvider theme="light">
-    {RouterProvider ? <RouterProvider /> : null}
-    <Error />
-  </ThemeProvider>
-)
+const App: FC<Props> = ({ RouterProvider }) => {
+  useEffect(() => {
+    const onPressKey = (event: KeyboardEvent) => {
+      if (event.ctrlKey && event.key === 'Enter') {
+        toggleFullScreen(null)
+      }
+    }
+
+    document.addEventListener('keyup', onPressKey)
+
+    return () => {
+      document.removeEventListener('keyup', onPressKey)
+    }
+  }, [])
+
+  return (
+    <ThemeProvider theme="light">
+      {RouterProvider ? <RouterProvider /> : null}
+      <Error />
+    </ThemeProvider>
+  )
+}
 
 export default withAuthInfo(App)
