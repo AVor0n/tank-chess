4b358b1a6616ad237a5fca5f080338362c467cbc
vetalvakulov <v.vakulov@bftcom.com>
2024-01-28 16:21:23 +0300

    feat(SSR): #Y36 Настроить Redux и Router в SSR. Фикс TS

--- packages/client/src/utils/renderObject.ts
+++ packages/client/src/utils/renderObject.ts
@@ -1,3 +1,3 @@
 import serialize from 'serialize-javascript'
 
-export const renderObject = (data: unknown) => serialize(data).replace(/</g, '\\\u003c')
+export const renderObject: (data: Record<string, string>) => string = data => serialize(data).replace(/</g, '\\\u003c')
