19abad744c1729e459f9e9be586be250af9f447d
Andrey Voronin <AVor0n@yandex.ru>
2024-01-31 13:31:27 +0300

    fix(app): #Y-61 Не открывается страница Профиль

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -3,17 +3,11 @@ import '@gravity-ui/uikit/styles/styles.css'
 import './assets/styles/index.css'
 import { Provider } from 'react-redux'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import store from 'store'
 import App from './app'
 import routes from './router'
-import { createStoreWithData, type RootStateType } from './store'
-
-const preloadState = window.__PRELOADED_STATE__
-delete window.__PRELOADED_STATE__
 
 const router = createBrowserRouter(routes)
-const preloadedState = preloadState?.preloadedState
-const store = createStoreWithData(preloadedState as RootStateType)
-
 const Router = () => <RouterProvider router={router} />
 
 ReactDOM.hydrateRoot(
--- packages/client/src/store.ts
+++ packages/client/src/store.ts
@@ -1,18 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit'
 import rootReducer from './reducers'
 
+const preloadedState = window.__PRELOADED_STATE__
+delete window.__PRELOADED_STATE__
+
 const store = configureStore({
   reducer: rootReducer,
+  devTools: process.env.NODE_ENV !== 'production',
+  preloadedState,
 })
 
-const createStoreWithData = (preloadedState: ReturnType<typeof store.getState>) =>
-  configureStore({
-    reducer: rootReducer,
-    devTools: process.env.NODE_ENV !== 'production',
-    preloadedState,
-  })
-
 export default store
-export { createStoreWithData }
 export type RootStateType = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
