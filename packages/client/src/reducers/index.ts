6865149a0e47252e8679e8b8e0f7e788fa186f8c
Vyaznikova Kristina <kristina-vyaz@yandex.ru>
2024-02-10 18:10:06 +0300

    feat(emoji): #Y44-45 Эмодзи  - клиентская и серверная части

--- packages/client/src/reducers/index.ts
+++ packages/client/src/reducers/index.ts
@@ -1,5 +1,4 @@
 import { combineReducers } from '@reduxjs/toolkit'
-import emojiSlice from './emoji'
 import errorSlice from './error'
 import getGameStartFinishDataSlice from './gameStartFinish'
 import userSlice from './user'
@@ -8,7 +7,6 @@ const rootReducer = combineReducers({
   user: userSlice,
   error: errorSlice,
   gameStartFinishData: getGameStartFinishDataSlice,
-  emojiBar: emojiSlice,
 })
 
 export default rootReducer
