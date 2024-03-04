d1a46ed4fbdcff5cd9c4dafdc4cd06bfb63db7ce
Marat Ikhsanov <m.ihsanov2009@yandex.ru>
2024-03-04 19:57:56 +0300

    Написать конфиг nginx (SSL) (#57)

--- packages/server/constants.ts
+++ packages/server/constants.ts
@@ -7,7 +7,10 @@ export const PORT = Number(process.env.SERVER_PORT) || 5000
 export const LOCAL_ORIGINS = [
   `http://127.0.0.1:5000`,
   `http://localhost:5000`,
-  'http://tank-chess.ya-praktikum.tech', // Заменить на тот, который получим с облака
+  `http://localhost`,
+  `http://127.0.0.1`,
+  'http://tank-chess.ya-praktikum.tech',
+  'https://tank-chess.ya-praktikum.tech',
 ]
 
 export const RESPONSE_MESSAGES = {
