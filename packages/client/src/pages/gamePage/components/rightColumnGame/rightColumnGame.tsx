6e558ba795c9d7cc96cf427835117e5b486b20b4
Andrey Voronin <AVor0n@yandex.ru>
2024-03-06 12:21:10 +0300

    feat(game): Уведомления о том что противник завершил игру

--- packages/client/src/pages/gamePage/components/rightColumnGame/rightColumnGame.tsx
+++ packages/client/src/pages/gamePage/components/rightColumnGame/rightColumnGame.tsx
@@ -2,19 +2,28 @@ import { NavLink } from 'react-router-dom'
 import { type Game } from 'lib/chess'
 import { resetGame } from 'reducers/game'
 import { useAppDispatch, useAppSelector } from 'reducers/hooks'
+import wsService from 'service/socket.service'
 import { GameInfo } from '../info'
 import SoundToggle from '../soundToggle'
 import styles from './rightColumnGame.module.scss'
 
 export const RightColumnGame = ({ game }: { game: Game }) => {
   const dispatch = useAppDispatch()
+  const userId = useAppSelector(state => state.auth.userInfo!.id)
+  const roomId = useAppSelector(state => state.game.roomId)
   const { sound } = useAppSelector(state => state.sound)
+
+  const endGame = () => {
+    dispatch(resetGame())
+    roomId && wsService.socket.emit('sent-room-event', { event: 'endGame', roomId, userId })
+  }
+
   return (
     <div className={styles.rightColumnGame}>
       <GameInfo game={game} />
       <div className={styles.rightColumn}>
         <SoundToggle withSound={sound} />
-        <NavLink onClick={() => dispatch(resetGame())} className={styles.exit} to="/">
+        <NavLink onClick={endGame} className={styles.exit} to="/">
           Завершить игру
         </NavLink>
       </div>
